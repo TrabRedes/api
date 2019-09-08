055e0e2ea76c1512375a95c6895536680d16cab4
Mauricio Milano <mauricio.milano@ibm.com>
2019-09-07 22:17:26 -0300

    render de tela do chat

--- src/index.js
+++ src/index.js
@@ -3,16 +3,21 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const authRouter = require('./routers/authRouter');
-
+const rootRouter = require ('./routers/root')
+const PORT = process.env.PORT || '3000'
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
 
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 app.use('/auth', authRouter);
+app.use('/',rootRouter );
 
-http.listen(process.env.PORT, () => {
-    console.log(`Server listening on ${process.env.PORT}...`);
+http.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}...`);
 });
+const io = require('socket.io')(http);
+io.on('connection',(socket)=>{
+    console.log('conectado')
+})
