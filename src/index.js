efd3734b5765599481192902307200a489db97b1
Mauricio Milano <mauricio.milano@ibm.com>
2019-09-08 11:25:55 -0300

    config de ambiente para deploy

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const authRouter = require('./routers/authRouter');
+<<<<<<< HEAD
 const rootRouter = require ('./routers/root');
 const dashboardRouter = require ('./routers/dashboardRouter');
 const Message = require('./models/message');
 
+=======
+const rootRouter = require ('./routers/root')
+>>>>>>> parent of 2658f21... chat funcionando com salvamento de mensagens no banco
 const PORT = process.env.PORT || '3000'
 const app = express();
 const http = require('http').Server(app);
@@ -37,14 +41,17 @@ io.on('connection', (socket) => {
     socket.on('new_message', (data) => {
         //broadcast the new message
         
-        io.sockets.emit('new_message', {message : data.message, username : socket.username, token: data.token});
+        io.sockets.emit('new_message', {message : data.message, username : socket.username});
     })
 
     //listen on typing
     socket.on('typing', (data) => {
     	socket.broadcast.emit('typing', {username : socket.username})
     })
+<<<<<<< HEAD
     socket.on('save_on_database', (data) => {
         Message.create(data);
     })
+=======
+>>>>>>> parent of 2658f21... chat funcionando com salvamento de mensagens no banco
 })
--- src/public/views/root/chat.js
+++ src/public/views/root/chat.js
@@ -1,5 +1,6 @@
 $(function(){
     //make connection
+<<<<<<< HEAD
     var socket = io.connect('http://localhost:3333')
     //buttons and inputs
     var message = $("#message")
@@ -26,22 +27,25 @@ $(function(){
     socket.emit('save_on_database',data) 
     
 }
+=======
+ var socket = io.connect('http://localhost:3333')
+
+ //buttons and inputs
+ var message = $("#message")
+ var username = $("#username")
+ var send_message = $("#send_message")
+ var send_username = $("#send_username")
+ var chatroom = $("#chatroom")
+ var feedback = $("#feedback")
+ var emitName = socket.emit('change_username',{username:'Mauricio'})
+>>>>>>> parent of 2658f21... chat funcionando com salvamento de mensagens no banco
  //Emit message
  send_message.click(function(){
-    const data = {
-        username: usernameVal, 
-        message : message.val(),
-        token:Math.random()*10000000 
-    }
-    debugger;
-    messageEvent(data, 'enviada')
-    socket.emit('new_message', {message : message.val(), token:data.token })
+     socket.emit('new_message', {message : message.val()})
  })
 
  //Listen on new_message
  socket.on("new_message", (data) => {
-     debugger
-     messageEvent(data, 'recebida')
      feedback.html('');
      message.val('');
      chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
@@ -49,7 +53,6 @@ $(function(){
 
  //Emit a username
  send_username.click(function(){
-     usernameVal = username.val();
      socket.emit('change_username', {username : username.val()})
  })
 
@@ -61,6 +64,11 @@ $(function(){
  //Listen on typing
  socket.on('typing', (data) => {
      feedback.html("<p><i>" + data.username + " is typing a message..." + "</i></p>")
+<<<<<<< HEAD
  }) 
 
 });
+=======
+ })
+});
+>>>>>>> parent of 2658f21... chat funcionando com salvamento de mensagens no banco
