746041658398ab3bf2d308475cb32050d36a6f40
Mauricio Milano <mauricio.milano@ibm.com>
2019-09-08 11:51:54 -0300

    merge corrigido

--- src/public/views/root/chat.js
+++ src/public/views/root/chat.js
@@ -1,35 +1,6 @@
 $(function(){
-    //make connection
-<<<<<<< HEAD
-    var socket = io.connect('http://localhost:3333')
-    //buttons and inputs
-    var message = $("#message")
-    var username = $("#username")
-    var send_message = $("#send_message")
-    var send_username = $("#send_username")
-    var chatroom = $("#chatroom")
-    var feedback = $("#feedback")
-    let usernameVal = 'Anonymous'
-    
-    
-    
-    /**
-     * data = {
-     *   userToken;
-     *   tokenMessage: implementada no futuro
-     *   type: received|sent 
-     * }
-     */
- const messageEvent = (data, status)=>{
-    data.time = new Date().toISOString();
-    data.status = status
-    debugger
-    socket.emit('save_on_database',data) 
-    
-}
-=======
+ //make connection
  var socket = io.connect('http://localhost:3333')
-
  //buttons and inputs
  var message = $("#message")
  var username = $("#username")
@@ -37,15 +8,29 @@ $(function(){
  var send_username = $("#send_username")
  var chatroom = $("#chatroom")
  var feedback = $("#feedback")
- var emitName = socket.emit('change_username',{username:'Mauricio'})
->>>>>>> parent of 2658f21... chat funcionando com salvamento de mensagens no banco
+ let usernameVal = 'Anonymous'
+
+
+ const messageEvent = (data, status)=>{
+    data.time = new Date().toISOString();
+    data.status = status
+    socket.emit('save_on_database',data) 
+    
+}
  //Emit message
  send_message.click(function(){
-     socket.emit('new_message', {message : message.val()})
+    const data = {
+        username: usernameVal, 
+        message : message.val(),
+        token:Math.random()*10000000 
+    }
+    messageEvent(data, 'enviada')
+    socket.emit('new_message', {message : message.val(), token:data.token })
  })
 
  //Listen on new_message
  socket.on("new_message", (data) => {
+     messageEvent(data, 'recebida')
      feedback.html('');
      message.val('');
      chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
@@ -53,6 +38,7 @@ $(function(){
 
  //Emit a username
  send_username.click(function(){
+     usernameVal = username.val();
      socket.emit('change_username', {username : username.val()})
  })
 
@@ -64,11 +50,6 @@ $(function(){
  //Listen on typing
  socket.on('typing', (data) => {
      feedback.html("<p><i>" + data.username + " is typing a message..." + "</i></p>")
-<<<<<<< HEAD
  }) 
 
 });
-=======
- })
-});
->>>>>>> parent of 2658f21... chat funcionando com salvamento de mensagens no banco
